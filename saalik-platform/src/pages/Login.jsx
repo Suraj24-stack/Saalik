bec5022e65c5ac45e9f136ff9441ffe5423cf1ad
hazekiller <hazekiller@example.com>
2025-10-18 21:24:49 +0545

    removing page

--- saalik-platform/src/pages/Login.jsx
+++ saalik-platform/src/pages/Login.jsx
@@ -346,16 +346,7 @@ export default function Login() {
             </form>
 
             {/* Divider */}
-            <div className="relative">
-              <div className="absolute inset-0 flex items-center">
-                <div className="w-full border-t border-gray-200"></div>
-              </div>
-              <div className="relative flex justify-center text-sm">
-                <span className="px-4 bg-white text-gray-500 font-medium">
-                  New to Maansu Education?
-                </span>
-              </div>
-            </div>
+            
 
             {/* Sign Up */}
             <div className="space-y-4">
